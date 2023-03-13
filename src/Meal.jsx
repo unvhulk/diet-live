3ae694ef9468a6c2fb536b13b24c1284383f4664
Uday Narayan Vishwakarma <unvhulk@gmail.com>
2023-03-13 14:47:35 +0530

    Fix : Picture position

--- src/Meal.jsx
+++ src/Meal.jsx
@@ -17,36 +17,46 @@ export const Meal = ({ meal }) => {
 						}}>
 						<div className=''>No. : {i + 1}</div>
 						<div className=''>Name : {meal.name}</div>
-						<img
-							src={meal.co_image}
-							alt=''
-							style={{ heigh: "100%", width: "100%" }}
-						/>
-						<div className=''>Calories : {meal.cal}</div>
-						<div className=''>Carbs Calories : {meal.carbs_cal}</div>
-						<div className=''>Carbs Grams : {meal.carbs_grams}</div>
-						<div className=''>Fat Calories : {meal.fat_cal}</div>
-						<div className=''>Fat Grams : {meal.fat_grams}</div>
-						<div className=''>Protien Calories : {meal.protien_cal}</div>
-						<div className=''>Protien Grams : {meal.protien_grams}</div>
-						<div className=''>Total Calories : {meal.totalCal}</div>
-						<div className=''>Total Grams : {meal.totalGrams}</div>
-						<div className=''>
-							Total Carbs Calories : {meal.total_carbs_cal}
-						</div>
-						<div className=''>Total Carbs Grams : {meal.total_carbs_grams}</div>
-						<div className=''>Total Fat Calories : {meal.total_fat_cal}</div>
-						<div className=''>Total Fat Grams : {meal.total_fat_grams}</div>
-						<div className=''>
-							Total Protien Calories : {meal.total_protien_cal}
-						</div>
-						<div className=''>
-							Total Protien Grams : {meal.total_protien_grams}
-						</div>
-						<div className=''>Total In Calories : {meal.total_in_cal}</div>
-						<div className=''>Total In Grams : {meal.total_in_grams}</div>
-						<div>
-							<Method method={meal.method} />
+						<div style={{ display: "grid", gridTemplateColumns: "1fr 2fr" }}>
+							<div className=''>
+								<img
+									src={meal.co_image}
+									alt=''
+									style={{ heigh: "100%", width: "100%" }}
+								/>
+							</div>
+							<div className=''>
+								<div className=''>Calories : {meal.cal}</div>
+								<div className=''>Carbs Calories : {meal.carbs_cal}</div>
+								<div className=''>Carbs Grams : {meal.carbs_grams}</div>
+								<div className=''>Fat Calories : {meal.fat_cal}</div>
+								<div className=''>Fat Grams : {meal.fat_grams}</div>
+								<div className=''>Protien Calories : {meal.protien_cal}</div>
+								<div className=''>Protien Grams : {meal.protien_grams}</div>
+								<div className=''>Total Calories : {meal.totalCal}</div>
+								<div className=''>Total Grams : {meal.totalGrams}</div>
+								<div className=''>
+									Total Carbs Calories : {meal.total_carbs_cal}
+								</div>
+								<div className=''>
+									Total Carbs Grams : {meal.total_carbs_grams}
+								</div>
+								<div className=''>
+									Total Fat Calories : {meal.total_fat_cal}
+								</div>
+								<div className=''>Total Fat Grams : {meal.total_fat_grams}</div>
+								<div className=''>
+									Total Protien Calories : {meal.total_protien_cal}
+								</div>
+								<div className=''>
+									Total Protien Grams : {meal.total_protien_grams}
+								</div>
+								<div className=''>Total In Calories : {meal.total_in_cal}</div>
+								<div className=''>Total In Grams : {meal.total_in_grams}</div>
+								<div>
+									<Method method={meal.method} />
+								</div>
+							</div>
 						</div>
 					</div>
 				))}
